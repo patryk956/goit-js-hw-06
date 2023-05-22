5a2ce6582991b5755cc0306b085be423cddecdd6
Patryk Lenczewski <118841977+patryk956@users.noreply.github.com>
2023-05-22 16:03:07 +0200

    Add files via upload

---
+++ task-09.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector("body");
+
+const changeColorEl = document.querySelector(".change-color");
+
+const colorEl = document.querySelector(".color");
+
+changeColorEl.addEventListener("click", changeBgColorRandom);
+
+function changeBgColorRandom() {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorEl.textContent = randomColor;
+}
